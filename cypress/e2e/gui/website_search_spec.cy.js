2014aa156e50a4c648b691c927a6432273c0c45c
jpmoreira-ti <jpmoreira.ti@gmail.com>
2024-12-19 10:08:55 -0300

    [tec] Adjusts on code

--- cypress/e2e/gui/website_search_spec.cy.js
+++ cypress/e2e/gui/website_search_spec.cy.js
@@ -1,11 +1,14 @@
 // @gui_search
 describe('Search', () => {
   it('successfully', () => {
+    //Act 
     cy.visit('/')
 
     cy.get('#search-input-templates')
+      .should('be.visible')
       .type('Cypress')
 
+    // Assert 
     cy.get('#cypress')
       .should('be.visible')
       .and('contain', 'Cypress')
